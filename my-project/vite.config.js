2b42c0fe79d5c65e892894c19b51291f1b4517f0
ilanchezhian-m <ilanchezhian27@gmail.com>
2026-01-07 11:30:13 +0530

    update on errors

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url === '/favicon.ico') {
+        res.statusCode = 302
+        res.setHeader('Location', '/favicon.svg')
+        res.end()
+        return
+      }
+      next()
+    })
+  },
 })
